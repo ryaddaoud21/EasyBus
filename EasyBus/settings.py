d3865dcfd6ddd381520d24b8d2d08bb3b2c65995
Your Name <you@example.com>
2023-10-03 12:25:02 +0200

    Postegrsql

--- EasyBus/settings.py
+++ EasyBus/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-mle)70h(*xh2-q#82i=64qli7heu$___qx9zy_^5grp66&2u!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+PORT = 8000 # Remplacez 8000 par le numéro de port souhaité
 
 # Application definition
 
@@ -111,14 +111,16 @@ WSGI_APPLICATION = 'EasyBus.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EASYBUS_TEST',
+        'USER': 'postgres',
+        'PASSWORD': '180175',
+        'HOST': 'localhost',  # Adresse de la base de données (peut être différente)
+        'PORT': '5432',       # Port de la base de données (par défaut pour PostgreSQL)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
